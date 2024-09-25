49e40ebc548d5bae7948cd9c31406a3d6e58738c
Rehan Pinjari <pinjarirehan11@gmail.com>
2024-09-25 13:01:22 +0530

    Added: Welcome Message is added

---
+++ src/com/bl/java/lambdaexpression/MathOperationApp.java
@@ -0,0 +1,9 @@
+package com.bl.java.lambdaexpression;
+
+public class MathOperationApp {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Math Operation App");
+    }
+}
