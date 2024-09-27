31610a4e953190e97c3e1d339ed9576c78e3a7f8
Rehan Pinjari <pinjarirehan11@gmail.com>
2024-09-27 15:33:33 +0530

    Added: Print Even Numbers Using Functional Interface

--- src/com/bl/java/lambdaexpression/MathOperationApp.java
+++ src/com/bl/java/lambdaexpression/MathOperationApp.java
@@ -2,29 +2,23 @@ package com.bl.java.lambdaexpression;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
+class NumberPlaylist {
+    private List<Integer> numbers;
 
-    //Functional interface for conversion
-    @FunctionalInterface
-    interface NumberConverter {
-        double convert(int number);
+    public NumberPlaylist() {
+        numbers = new ArrayList<>();
     }
 
-    class NumberPlaylist {
-        private List<Integer> numbers;
-
-        public NumberPlaylist() {
-            numbers = new ArrayList<>();
-        }
-
-        public void addNumber(int number) {
-            numbers.add(number);
-        }
-
-        public List<Integer> getNumbers() {
-            return numbers;
-        }
+    public void addNumber(int number) {
+        numbers.add(number);
     }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
 
 public class MathOperationApp {
     public static void main(String[] args) {
@@ -36,13 +30,15 @@ public class MathOperationApp {
         playlist.addNumber(4);
         playlist.addNumber(5);
 
-        //Define the conversion using a lambda expression
-        NumberConverter toDouble = number -> (double) number;
+        // Define the Predicate using a lambda expression to check for even numbers
 
-        System.out.println("Converting numbers to double and printing:");
+        Predicate<Integer> isEven = number -> number % 2 == 0;
+
+        System.out.println("Printing only Even Numbers");
         playlist.getNumbers().forEach(number -> {
-            double convertedNumber = toDouble.convert(number);
-            System.out.println("Number as Double: " + convertedNumber);
+            if (isEven.test(number)) {
+                System.out.println("Even Number: " + number);
+            }
         });
     }
 }
